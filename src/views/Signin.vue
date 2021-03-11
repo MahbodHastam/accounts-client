52ad61384547dc48aaf8ad9ada08d6c2f2d0639e
= <pesaregoal@gmail.com>
2021-03-12 00:16:56 +0330

    divide sign in view

---
+++ src/views/Signin.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="home">
+    <Login />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Login from '@/components/Login.vue'
+
+export default {
+  name: 'Signin',
+  components: {
+    Login
+  }
+}
+</script>
+
+<style scoped>
+.home {
+  padding: 16px 8px !important;
+}
+</style>
