e66d8469397dd717851bf0dde11b38738dc86d77
= <pesaregoal@gmail.com>
2021-03-12 13:47:42 +0330

    update

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,21 @@
 <template>
-    <h3>Welcome {{this.name}} {{this.lastname}}</h3>
+    <div>
+        <div id="avatar"></div>
+        <h3>Welcome {{this.name}} {{this.lastname}}</h3>
+        <div class="section">
+            <h4>Privacy & Personalization</h4>
+            <p>choose what activity is saved to personalize your Myren experience!</p>
+            <router-link to="/data">see more >></router-link>
+        </div>
+        <div class="section">
+            <h4>We value your security</h4>
+            <p>The security service is designed to give you personalized recommendations to secure your accounnts and wallets</p>
+            <router-link to="">check up >></router-link>
+        </div>
+        <div class="outer">
+            <p>Only you can see your settings. You might also want to review your settings for Audiofy, Vidible, or whichever Myren services you use most. Myren keeps your data private, safe, and secure</p>
+        </div>
+    </div>
 </template>
 <script>
 import axios from 'axios'
@@ -25,6 +41,39 @@ export default {
 <style scoped>
 h3 {
     text-align: center;
-    margin-top: 24px;
+    margin-top: 16px;
+    margin-bottom: 16px;
+}
+.section {
+    margin: 32px 16px;
+    border: 0.01rem solid #d6d6d6;
+    padding: 10px 16px;
+    border-radius: 12px;
+}
+.outer {
+    margin: 32px 16px;
+    padding: 10px 16px;
+}
+.outer p {
+    color: #8b8b8b;
+}
+#avatar {
+    width: 80px;
+    height: 80px;
+    background: linear-gradient(45deg, #2a2a2a, #5e5e5e);
+    border-radius: 50%;
+    margin: 16px auto 0 auto;
+}
+p{
+    margin-top: 10px;
+    line-height: 22px;
+}
+a{
+    display: block;
+    margin-top: 10px;
+    text-decoration: none;
+    font-weight: bold;
+    font-family: 'Poppins';
+    color: rgb(0, 60, 255);
 }
 </style>
