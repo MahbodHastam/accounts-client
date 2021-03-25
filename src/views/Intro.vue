1766ce4e4172fc72ea9a1e2268b303ef8f0c4d2a
= <pesaregoal@gmail.com>
2021-03-25 14:48:42 +0430

    update

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,5 +1,29 @@
 <template>
   <div class="intro">
-    <p>this is intro page</p>
+    <h3>One For All</h3>
+    <p>in order to use Myren products you need to log in first, as soon as you log in, your info will sync up to all of our service, in other words log in once use many times</p>
+    <router-link to="/signin">sign in >></router-link>
   </div>
 </template>
+
+<style scoped>
+.intro{
+  padding: 16px;
+}
+h3 {
+  margin-top: 16px;
+  margin-bottom: 16px;
+}
+p {
+  margin-top: 10px;
+  line-height: 22px;
+}
+a {
+  display: block;
+  margin-top: 10px;
+  text-decoration: none;
+  font-weight: bold;
+  font-family: 'Poppins';
+  color: rgb(0, 60, 255);
+}
+</style>
