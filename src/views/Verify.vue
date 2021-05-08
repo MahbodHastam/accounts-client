3ea6d8d09b29b456c623eab2dc468fdabf881b6b
= <pesaregoal@gmail.com>
2021-05-08 21:03:14 +0430

    fix route after signing in

--- src/views/Verify.vue
+++ src/views/Verify.vue
@@ -59,15 +59,17 @@ export default {
               })
               .then(response => {
                 //   console.log(response.data)
-                if (response.data.user_firstname == "Guest" && response.data.user_lastname == "User") {
-                  self.$router.push('completeprofile')
+                if (
+                  response.data.user_firstname == 'Guest' &&
+                  response.data.user_lastname == 'User'
+                ) {
+                  self.$router.push('editprofile')
                 } else {
                   self.$router.push('/')
                 }
               })
               .catch(error => console.log(error))
               .then(() => {})
-            
           }
         })
         .catch(error => console.log(error))
