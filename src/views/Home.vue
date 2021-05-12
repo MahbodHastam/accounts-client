b06b8b8e9fed8ece80ebbf73df8497dd919e6dad
= <pesaregoal@gmail.com>
2021-05-12 19:32:53 +0430

    route to signin instead of into

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -54,7 +54,8 @@ export default {
         if (response.data.ok) {
           self.$store.commit('updateUserInfo', response.data)
         } else {
-          self.$router.push('/intro')
+          // self.$router.push('/intro')
+          self.$router.push('/signin')
         }
         //   self.$store.state
       })
